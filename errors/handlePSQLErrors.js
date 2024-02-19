40f0ff21945cd1475247d5e11d5eae9122553eb0
M1nhnho <tien.nguho@hotmail.com>
2024-02-19 16:46:05 +0000

    Added missing sad path tests for 'POST /api/articles/:article_id/comments'.

--- errors/handlePSQLErrors.js
+++ errors/handlePSQLErrors.js
@@ -3,9 +3,13 @@ function handlePSQLErrors(error, request, response, next)
     // 22P02: INVALID TEXT REPRESENTATION
     // 23502: NOT NULL VIOLATION
     // 23503: FOREIGN KEY VIOLATION
-    if (error.code === '22P02' || error.code === '23502' || error.code === '23503')
+    if (error.code === '22P02' || error.code === '23502')
     {
         response.status(400).send({msg: 'Bad Request'});
+    }
+    else if (error.code === '23503')
+    {
+        response.status(404).send({msg: 'Not Found'});
     }
     else
     {
