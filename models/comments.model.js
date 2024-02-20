186149e9d1c45966b0c4ec6c2703933527455554
M1nhnho <tien.nguho@hotmail.com>
2024-02-20 11:23:22 +0000

    Small reformatting with indentation.

--- models/comments.model.js
+++ models/comments.model.js
@@ -3,11 +3,12 @@ const db = require('../db/connection.js');
 exports.selectCommentsByArticleID = (articleID) =>
 {
     return db.query(
-        `SELECT *
-            FROM comments
-            WHERE article_id = $1
-            ORDER BY created_at DESC;`,
-        [articleID])
+            `SELECT *
+                FROM comments
+                WHERE article_id = $1
+                ORDER BY created_at DESC;`,
+            [articleID]
+        )
         .then(({ rows }) =>
         {
             return rows;
@@ -17,12 +18,13 @@ exports.selectCommentsByArticleID = (articleID) =>
 exports.insertCommentAtArticleID = (username, body, articleID) =>
 {
     return db.query(
-        `INSERT INTO comments
-            (body, author, article_id, votes, created_at)
-            VALUES
-                ($1, $2, $3, $4, $5)
-            RETURNING *;`,
-        [body, username, articleID, 0, new Date(Date.now())])
+            `INSERT INTO comments
+                (body, author, article_id, votes, created_at)
+                VALUES
+                    ($1, $2, $3, $4, $5)
+                RETURNING *;`,
+            [body, username, articleID, 0, new Date(Date.now())]
+        )
         .then(({ rows }) =>
         {
             return rows[0];
