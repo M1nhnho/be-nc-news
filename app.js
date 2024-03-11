c7cd848634549138949273588765db070fa80ba6
M1nhnho <tien.nguho@hotmail.com>
2024-03-11 10:21:35 +0000

    Allowed cross origin resource sharing.

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const { handleEndpointErrors, handlePSQLErrors, handleCustomErrors, handleServerErrors } = require('./errors');
 const apiRouter = require('./routes/apiRouter.js');
+const cors = require('cors');
 
 // Server
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use('/api', apiRouter);
